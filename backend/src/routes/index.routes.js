98d9caf7b18d94b5ba50d7e1d2b3047d6ca5dacd
Sergio <sergioguzmanm123@gmail.com>
2025-07-16 19:48:25 -0400

    Se agrego Transaction y inventory

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,10 +2,14 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js"
 import userRoutes from "./user.routes.js";
+import TransactionRoutes from "./transaction.routes.js";
+import InventoryRoutes from "./inventory.routes.js";
 
 const router = new Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/transaction", TransactionRoutes);
+router.use("/inventory", InventoryRoutes);
 
 export default router;
