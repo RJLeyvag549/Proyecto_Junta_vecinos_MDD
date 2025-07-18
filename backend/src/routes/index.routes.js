6c832cec81a15af83c3db7bf1cf7e2ca1e2ed8a1
vvnao <valentina.martinez2302@alumnos.ubiobio.cl>
2025-07-18 19:24:08 -0400

    Refactor voting functionality: enhance emitirVoto to include user validation and add contarVotosPorOpcion for vote counting; update routes for improved structure and clarity.

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,13 +3,13 @@ import { Router } from "express";
 import authRoutes from "./auth.routes.js"
 import userRoutes from "./user.routes.js";
 import votacionRoutes from "./votacion.routes.js";
-// import votoRoutes from "./voto.routes.js";
+import votoRoutes from "./voto.routes.js";
 
 const router = new Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/votacion", votacionRoutes);
-// router.use("/voto", votoRoutes);
+router.use("/voto", votoRoutes);
     
 export default router;
