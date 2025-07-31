3c47c58e1603e2295221c455de8766b826adee05
tsolrven <vicaria.silvana@gmail.com>
2025-07-31 18:19:45 -0400

    me traje el fron

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,31 @@
+import axios from './root.service.js';
+import cookies from 'js-cookie';
+
+export async function login(data) {
+    const response = await axios.post('/auth/login', data);
+    if (response.status === 200) {
+        sessionStorage.setItem('usuario', JSON.stringify(response.data));
+    }
+    return response.data;
+}
+
+export async function register(data) {
+    const response = await axios.post('/auth/register', data);
+    return response.data;
+}
+
+export async function profile() {
+    const config = {
+        headers: {
+            'Cache-Control': 'no-cache',
+        },
+    };
+    const response = await axios.get('/auth/profile', config);
+    return response.data;
+}
+
+export async function logout() {
+    await axios.post('/auth/logout');
+    sessionStorage.removeItem('usuario');
+    cookies.remove('miCookie');
+}
