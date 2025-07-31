4207f08515da9afbd1bba911d0e1e25044acf8ea
sayen.barra2301@alumnos.ubiobio.cl <sayen.barra2301@alumnos.ubiobio.cl>
2025-07-30 23:25:48 -0400

    Resolviendo conflictos al mezclar con prueba

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:3000/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/auth/, '/auth'), // mantiene la ruta
+      },
+    },
+  },
+});
